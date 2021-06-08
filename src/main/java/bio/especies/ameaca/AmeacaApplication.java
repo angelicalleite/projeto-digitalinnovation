3d2ddeeab9411a0708484d64fd4aeef28eb7c291
Angelica Leite <angelica.santos@cnj.jus.br>
2021-06-08 00:01:20 -0300

    Inicialização do projeto

---
+++ src/main/java/bio/especies/ameaca/AmeacaApplication.java
@@ -0,0 +1,13 @@
+package bio.especies.ameaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeacaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmeacaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bio/especies/ameaca/AmeacaApplicationTests.java
@@ -0,0 +1,13 @@
+package bio.especies.ameaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeacaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
